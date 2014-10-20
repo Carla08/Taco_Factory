a7b8d017e564e639cb466b0ad2e9270de6802609
Carla08 <dcfc@live.com.mx>
2014-10-20 01:12:58 -0600

    Working. Perfection. DONE!

--- src/Tacos_Factory.java
+++ src/Tacos_Factory.java
@@ -2,49 +2,55 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by mario_000 and Carla Prieto on 10/18/2014.
  */
 
 
-public class Tacos_Factory implements Runnable {
+public class Tacos_Factory {
     List<String> lista = new ArrayList<String>();//La lista de donde los tacos(el producto) se va a ir tomando
     Semaphore n;
     Semaphore s;
     int produced=0;
     int consumed=0;
-    int chef_velocity=0;
-    int maistro_velocity=0;
+    int chef_velocity;
+    int maistro_velocity;
+    JLabel cooked;
+    JLabel eated;
     
+        
+    public void Change_Chef_velocity(int vel){
+        chef_velocity=vel*45;
+    }
+    
+    public void Change_Maistro_velocity(int vel){
+        maistro_velocity=vel*45;
+    }
     /***tuve que crear un hilo para poder ejecutar los otros dos, sigase esto como un dogma de Fe
      ya que no podia llamar clases NO estaticas desde el main***/
     public static void main (String [] args){
         Tacos_Factory_GUI tacosGUI=new Tacos_Factory_GUI();
         tacosGUI.setVisible(true);
-        
-
-
     }
-  
-    public void startTacosFactory(int chefVelocity, int maistroVelocity){
+    
+    public void startTacosFactory(int chefVelocity, int maistroVelocity, JLabel cooked, JLabel eated){
         //y=-45x+5000
         //Lowest velocity 0=5000
         //Fastest velocity 100=500
-        this.chef_velocity=(-45*chefVelocity+5000);
-        this.maistro_velocity=(-45*maistroVelocity+5000);
-        new Thread( new Tacos_Factory()).start();
+        this.cooked=cooked;
+        this.eated=eated;
+        chef_velocity=(-45*chefVelocity+5000);
+        maistro_velocity=(-45*maistroVelocity+5000);
+        n=new Semaphore(0,true);
+        s=new Semaphore(1,true);
+        new Thread( new Producer()).start();
+        new Thread( new Consumer()).start(); 
     }
 
     /***Este metodo inicializa los semaphores y corre los hilos***/
-    @Override
-    public void run() {
-        n=new Semaphore(0,true);
-        s=new Semaphore(1,true);
-        new Thread(new Producer()).start();
-        new Thread(new Consumer()).start();
-    }
-
     /***El dios de los tacos empieza a crear tacos para los maistros***/
     class Producer implements Runnable {
 
@@ -62,7 +68,7 @@ public class Tacos_Factory implements Runnable {
                 n.release();
                 s.release();
                 try {
-                    Thread.sleep(chef_velocity);
+                    Thread.sleep(5000-chef_velocity);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -75,7 +81,8 @@ public class Tacos_Factory implements Runnable {
             produced++;
             String producto="taco"+Math.random()*100;
             System.out.println("Tacos producidos: "+produced);
-
+            cooked.setText(Integer.toString(produced));
+            System.out.println("Chef velocity: "+chef_velocity);
             return producto;
         }
 
@@ -107,7 +114,7 @@ public class Tacos_Factory implements Runnable {
                 s.release();
                 consume(producto);
                 try {
-                    Thread.sleep(maistro_velocity);
+                    Thread.sleep(5000-maistro_velocity);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -122,6 +129,7 @@ public class Tacos_Factory implements Runnable {
             }
             String producto=lista.remove(0);
             System.out.println("Se ha tomado un: "+producto);
+            System.out.println("Maistro velocity:"+maistro_velocity);
             return producto;
         }
 
@@ -129,6 +137,7 @@ public class Tacos_Factory implements Runnable {
         public void consume(String producto){
             consumed++;
             System.out.println("Tacos consumidos: "+consumed);
+            eated.setText(Integer.toString(consumed));
         }
     }
 }
